9dcb408234b723ee6205620815b9dfa44e40b692
samlifei <samlfei@163.com>
2019-03-29 10:10:25 +0800

    0.0.2

---
+++ TTXCVPN/TTXCVPNDemo/TTXCVPNSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TTXCVPNSDK
+//
+//  Created by ttxc on 2019/3/20.
+//  Copyright © 2019 http://www.jianshu.com/u/84d0142cbec9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
