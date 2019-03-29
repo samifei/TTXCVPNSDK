29581e8185d31a3ebe6ca497670b1e51f7b7a825
samlifei <samlfei@163.com>
2019-03-29 10:03:18 +0800

    1.2

---
+++ TTXCVPN/TTXCVPNDemo/TTXCVPNSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTXCVPNSDK
+//
+//  Created by ttxc on 2019/3/20.
+//  Copyright © 2019 http://www.jianshu.com/u/84d0142cbec9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
